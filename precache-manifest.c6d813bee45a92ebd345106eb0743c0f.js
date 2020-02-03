8d10065500fd26e9ed0c6b1b663a7faadbf349b6
Ryan Jamison <ryanjamison26@gmail.com>
2020-02-03 11:00:16 -0500

    Updates

---
+++ precache-manifest.c6d813bee45a92ebd345106eb0743c0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c1689af12f70c411f9fe5770028bb63",
+    "url": "/ExerTrack/index.html"
+  },
+  {
+    "revision": "545e1370c9eb4bfa5dbd",
+    "url": "/ExerTrack/static/css/2.23be29d4.chunk.css"
+  },
+  {
+    "revision": "545e1370c9eb4bfa5dbd",
+    "url": "/ExerTrack/static/js/2.df1516c5.chunk.js"
+  },
+  {
+    "revision": "299cba5f6c747cbc295097594263041b",
+    "url": "/ExerTrack/static/js/2.df1516c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "742eac9df2ce4682a368",
+    "url": "/ExerTrack/static/js/main.f5f647a3.chunk.js"
+  },
+  {
+    "revision": "5b889d66ce0c3a82e213",
+    "url": "/ExerTrack/static/js/runtime-main.dae3a494.js"
+  }
+]);
